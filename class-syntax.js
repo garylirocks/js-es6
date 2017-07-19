d718a50e7c1d5ecd5ff26dbe5dae920fa09d0544
garylirocks <gary.li.rocks@gmail.com>
2017-07-19 14:44:36 +1200

    update demo code about class and default parameters

--- class-syntax.js
+++ class-syntax.js
@@ -13,10 +13,20 @@ class People {
 }
 
 class Men extends People {          // class inheritance
+    // static let arms = 2;         // NOTE this is not allowed, you can not declare a static property
+
+    static legs() {                 // NOTE a static method attached to the class directly
+        return 2;
+    }
+
     constructor(name, age) {
         super(name, age, 'Male');   // the 'super' keyword
     }
 }
 
+Men.arms = 2;                       // NOTE you can still add properties to classes in this way
+
 var gary = new Men('Gary', 30);
 gary.introduce();
+
+console.log(Men.legs());
--- default-parameter-value.js
+++ default-parameter-value.js
@@ -1,8 +1,30 @@
-/*
- * default value of parameters
- */
+// NOTE default practice, the same as other languages
 var nameBuilder = function(firstName="Garry", lastName="Doe") {
 	console.log(firstName + " " + lastName);
 };
 
 nameBuilder();
+
+
+// NOTE you can refer to other parameters in the default value
+function foo(price = 20, tax = price * 0.1) {
+	console.log("price: " + price);
+	console.log("tax: " + tax);
+}
+
+foo();
+
+
+// NOTE you can use a function as default value for a parameter as well
+let taxRate = (price) => 0.2 * price;
+function foo2(price = 20, tax = taxRate) {
+	console.log("price: " + price);
+	console.log("tax: " + taxRate(price));
+}
+
+foo2();
+
+
+// NOTE create a dynamic function with the `Function` constructor
+let foo3 = new Function("a = 2", "b = 3", "console.log(a * b);");
+foo3(5);
